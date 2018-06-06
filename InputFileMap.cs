ca030d14f3bc3290193e9b8853905899c887d280
alex <alexe_webmail@yahoo.com>
2018-06-06 18:13:58 -0400

    Add iteration and timestep to NetLogo module transformer and schema

---
+++ InputFileMap.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using SyncroSim.Common;
+using System.Globalization;
+
+namespace SyncroSim.NetLogo
+{
+    internal class InputFileMap
+    {
+        private SortedKeyMap2<string> m_Map = new SortedKeyMap2<string>(SearchMode.ExactPrev);
+
+        public void AddInputFile(Nullable<int> iteration, Nullable<int> timestep, string fileName)
+        {
+            string f = this.m_Map.GetItemExact(iteration, timestep);
+
+            if (f != null)
+            {
+                string s = string.Format(CultureInfo.InvariantCulture,
+                    "There is already a file for iteration {0} and timestep {1}", iteration, timestep);
+
+                throw new ArgumentException(s);
+            }
+
+            this.m_Map.AddItem(iteration, timestep, fileName);
+        }
+
+        public string GetInputFile(int iteration, int timestep)
+        {
+            return this.m_Map.GetItem(iteration, timestep);
+        }
+    }
+}
--- RuntimeTransformer.cs
+++ RuntimeTransformer.cs
@@ -10,111 +10,127 @@ namespace SyncroSim.NetLogo
 {
     class RuntimeTransformer : StochasticTimeTransformer
     {
+        private string m_ExeName;
+        private string m_JarFileName;
+        private DataSheet m_InputFiles;
+        private InputFileMap m_InputFileMap;
+        private string m_MinimumIteration;
+        private string m_MaximumIteration;
+        private string m_MinimumTimestep;
+        private string m_MaximumTimestep;
+        private string m_TemplateFileName;
+        private string m_ExperimentName;
+        private const string DEFAULT_NETLOGO_FOLDER = "NetLogo 6.0";
+        private const string NETLOGO_EXE_NAME = "NetLogo.exe";
+
         public override void Configure()
         {
             base.Configure();
 
+            DataSheet ds = this.ResultScenario.GetDataSheet("NetLogo_RunControl");
+            DataTable dt = ds.GetData();
 
-
-        }
-
-        public override void Transform()
-        {
-            try
+            if (ds.GetDataRow() == null)
             {
-                this.SetStatusMessage("Running NetLogo");
-                this.InternalTransform();
-            }
-            finally
-            {
-                this.SetStatusMessage(null);
+                dt.Rows.Add(dt.NewRow());
+                ds.Changes.Add(new ChangeRecord(this, "Added default run control data."));
             }
         }
 
-        private void InternalTransform()
+        public override void Initialize()
         {
-            string TemplateFileName = this.GetNetLogoTemplateFile();
+            base.Initialize();
 
-            if (this.IsUserInteractive())
+            this.InitializeRunControl();
+            this.InitializeExeName();
+            this.InitializeJarFileName();
+            this.InitializeInputFiles();
+        }
+
+        private void InitializeRunControl()
+        {
+            this.m_MinimumIteration = Convert.ToString(this.GetRunControlValue("MinimumIteration"));
+            this.m_MaximumIteration = Convert.ToString(this.GetRunControlValue("MaximumIteration"));
+            this.m_MinimumTimestep= Convert.ToString(this.GetRunControlValue("MinimumTimestep"));
+            this.m_MaximumTimestep = Convert.ToString(this.GetRunControlValue("MaximumTimestep"));
+            this.m_TemplateFileName = Convert.ToString(this.GetRunControlValue("TemplateFile"));
+            this.m_ExperimentName = Convert.ToString(this.GetRunControlValue("Experiment"));
+        }
+
+        private void InitializeExeName()
+        {
+            this.m_ExeName = this.GetExternalExecutableName(NETLOGO_EXE_NAME, DEFAULT_NETLOGO_FOLDER);
+
+            if (this.m_ExeName == null || !Path.IsPathRooted(this.m_ExeName))
             {
-                string Exe = this.GetNetLogoExeFile();
-                base.ExternalTransform(Exe, null, TemplateFileName, null);
+                throw new InvalidOperationException("Cannot find NetLogo.exe - please configure your library properties.");
+            }
+        }
+
+        private void InitializeJarFileName()
+        {
+            string AppDir = Path.Combine(Path.GetDirectoryName(this.m_ExeName), "app");
+            string[] Files = Directory.GetFiles(AppDir, "netlogo-*.jar");
+
+            if (Files.Length == 1)
+            {
+                this.m_JarFileName = Files[0];
             }
             else
             {
-                string JarFileName = this.GetNetLogoJarFile();
-                string Experiment = this.GetNetLogoExperiment();
+                throw new InvalidOperationException("Cannot find NetLogo Jar file in: " + AppDir);
+            }
+        }
 
+        void InitializeInputFiles()
+        {
+            this.m_InputFileMap = new InputFileMap();
+            this.m_InputFiles = this.ResultScenario.GetDataSheet("NetLogo_InputFile");
+            DataTable dt = this.m_InputFiles.GetData();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Nullable<int> Iteration = GetNullableInt(dr, "Iteration");
+                Nullable<int> Timestep = GetNullableInt(dr, "Timestep");
+                string Filename = (string)dr["Filename"];
+
+                this.m_InputFileMap.AddInputFile(Iteration, Timestep, Filename);
+            }
+        }
+
+        protected override void OnTimestep(int iteration, int timestep)
+        {
+            base.OnTimestep(iteration, timestep);
+
+            string TemplateFileName = this.CreateNetLogoTemplateFile();
+
+            if (this.IsUserInteractive())
+            {
+                base.ExternalTransform(this.m_ExeName, null, TemplateFileName, null);
+            }
+            else
+            {
                 string args = string.Format(CultureInfo.InvariantCulture,
                     "-Xmx1024m -Dfile.encoding=UTF-8 -cp \"{0}\" org.nlogo.headless.Main --model \"{1}\" --experiment {2}",
-                    JarFileName, TemplateFileName, Experiment);
+                    this.m_JarFileName, TemplateFileName, this.m_ExperimentName);
 
                 base.ExternalTransform("java", null, args, null);
             }
         }
 
-        private string GetNetLogoJarFile()
-        {
-            string f = this.GetJarFileNameFromConfig();
+        private string CreateNetLogoTemplateFile()
+        {       
+            string TempFolderName = this.Library.CreateTempFolder("NetLogo", true);
+            string f1 = this.GetRunControlFileName(this.m_TemplateFileName);
+            string f2 = Path.Combine(TempFolderName, this.m_TemplateFileName);
 
-            if (f == null)
+            if (!File.Exists(f1))
             {
-                f = this.GetDefaultJarFileName();
+                throw new InvalidOperationException("The NetLogo template file was not found.");
             }
 
-            if (f == null)
-            {
-                throw new InvalidOperationException("Cannot find the NetLogo Jar file!");
-            }
-
-            if (!File.Exists(f))
-            {
-                string s = string.Format(CultureInfo.InvariantCulture, "The NetLogo jar file does not exist: {0}", f);
-                throw new InvalidOperationException(s);
-            }
-
-            return f;
-        }
-
-        private string GetNetLogoExeFile()
-        {
-            string f = this.GetExeNameFromConfig();
-
-            if (f == null)
-            {
-                f = this.GetDefaultExeName();
-            }
-
-            if (f == null)
-            {
-                throw new InvalidOperationException("Cannot find the NetLogo EXE file!");
-            }
-
-            if (!File.Exists(f))
-            {
-                string s = string.Format(CultureInfo.InvariantCulture, "The NetLogo EXE file does not exist: {0}", f);
-                throw new InvalidOperationException(s);
-            }
-
-            return f;
-        }
-
-        private string GetNetLogoTemplateFile()
-        {
-            //string TemplateFileName = (string)this.GetRunControlValue("TemplateFile");
-            //string TempFolderName = this.Library.GetTempFolderName("NetLogo");
-            //string f1 = this.GetRunControlFileName(TemplateFileName);
-            //string f2 = Path.Combine(TempFolderName, TemplateFileName);
-
-            //if (!File.Exists(f1))
-            //{
-            //    throw new InvalidOperationException("The NetLogo template file was not found.");
-            //}
-
-            //this.WriteNetLogoTemplate(f1, f2);
-            //return f2;
-
-            return null;
+            this.WriteNetLogoTemplate(f1, f2);
+            return f2;
         }
 
         private void WriteNetLogoTemplate(string source, string target)
@@ -141,15 +157,14 @@ namespace SyncroSim.NetLogo
 
         private string ReplaceSymbols(string line)
         {
-            string l = line.Replace("%SSIM_MIN_TIMESTEP%", Convert.ToString(this.GetRunControlValue("MinimumTimestep")));
-            l = line.Replace("%SSIM_MAX_TIMESTEP%", Convert.ToString(this.GetRunControlValue("MaximumTimestep")));
+            string l = line;
+
+            l = l.Replace("%SSIM_MIN_ITERATION%", this.m_MinimumIteration);
+            l = l.Replace("%SSIM_MAX_ITERATION%", this.m_MaximumIteration);
+            l = l.Replace("%SSIM_MIN_TIMESTEP%", this.m_MinimumTimestep);
+            l = l.Replace("%SSIM_MAX_TIMESTEP%", this.m_MaximumTimestep);
 
             return (l);
-        }
-
-        private string GetNetLogoExperiment()
-        {
-            return (string)this.GetRunControlValue("Experiment");
         }
 
         private object GetRunControlValue(string columnName)
@@ -164,109 +179,18 @@ namespace SyncroSim.NetLogo
             return dr[columnName];
         }
 
-        private string GetExeNameFromConfig()
+        private int? GetNullableInt(DataRow dr, string columnName)
         {
-            DataRow dr = this.GetEnvironmentRow("SSIM_WINDOWS_EXECUTABLE_LOCATION");
+            object value = dr[columnName];
 
-            if (dr == null)
+            if (object.ReferenceEquals(value, DBNull.Value) || object.ReferenceEquals(value, null))
             {
                 return null;
             }
-
-            string v = (string)dr["Value"];
-            return v;
-        }
-
-        private string GetJarFileNameFromConfig()
-        {
-            string f = this.GetExeNameFromConfig();
-
-            if (f == null)
+            else
             {
-                return null;
+                return Convert.ToInt32(value, CultureInfo.InvariantCulture);
             }
-
-            f = Path.Combine(Path.GetDirectoryName(f), "app");
-            f = Path.Combine(f, "netlogo-6.0.0.jar");
-
-            return f;
-        }
-
-        private string GetDefaultExeName()
-        {
-            string f = this.GetDefaultNetLogoFolder();
-            return Path.Combine(f, "NetLogo.exe");
-        }
-
-        private string GetDefaultJarFileName()
-        {
-            string f = this.GetDefaultNetLogoFolder();
-
-            f = Path.Combine(f, "app");
-            f = Path.Combine(f, "netlogo-6.0.0.jar");
-
-            return f;
-        }
-
-        private string GetDefaultNetLogoFolder()
-        {
-            string f = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "NetLogo 6.0");
-
-            if (Directory.Exists(f))
-            {
-                return f;
-            }
-
-            f = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "NetLogo 6.0");
-
-            if (Directory.Exists(f))
-            {
-                return f;
-            }
-
-            //We only ship x64 builds, but it is nice to have this work on an x86
-            //development machine, so:
-
-            f = Path.Combine("C:\\Program Files", "NetLogo 6.0");
-
-            if (Directory.Exists(f))
-            {
-                return f;
-            }
-
-            f = Path.Combine("C:\\Program Files (x86)", "NetLogo 6.0");
-
-            if (Directory.Exists(f))
-            {
-                return f;
-            }
-
-            return null;
-        }
-
-        private DataRow GetEnvironmentRow(string keyName)
-        {
-            DataSheet ds = this.Library.GetDataSheet("NetLogo_Environment");
-
-            foreach (DataRow dr in ds.GetData().Rows)
-            {
-                if (dr.RowState == DataRowState.Deleted)
-                {
-                    continue;
-                }
-
-                if (dr["Name"] != DBNull.Value)
-                {
-                    string n = (string)dr["Name"];
-
-                    if (n == keyName)
-                    {
-                        return dr;
-                    }
-                }
-            }
-
-            return null;
         }
 
         private string GetRunControlFileName(string fileName)
