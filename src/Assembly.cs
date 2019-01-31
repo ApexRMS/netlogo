fba7d7e84239f6375902ab4d3a103e480add96d9
alex <alexe_webmail@yahoo.com>
2019-01-31 14:26:15 -0800

    Change 'Module' to 'Package' in file headers.

--- src/Assembly.cs
+++ src/Assembly.cs
@@ -1,4 +1,4 @@
-﻿//NetLogo: A SyncroSim Module for running NetLogo simulations.
+﻿//netlogo: A SyncroSim Package for running NetLogo simulations.
 //Copyright © 2007-2019 Apex Resource Management Solution Ltd. (ApexRMS). All rights reserved.
 
 using System;
